a6733609bb5d70ebe70a219aebee6b32a974ba40
tai <dinhtai0293@gmail.com>
2022-06-15 07:34:48 +0700

    up

---
+++ js/js.js
@@ -0,0 +1,45 @@
+// html
+var js_h3 = document.querySelectorAll('h3')
+var content = document.querySelectorAll('.content')
+js_h3[0].onclick = function () {
+    content[0].classList.toggle('show')
+}
+content[0].onclick = function () {
+    content[0].classList.toggle('show')
+}
+js_h3[1].onclick = function () {
+    content[1].classList.toggle('show')
+}
+content[1].onclick = function () {
+    content[1].classList.toggle('show')
+}
+js_h3[2].onclick = function () {
+    content[2].classList.toggle('show')
+}
+content[2].onclick = function () {
+    content[2].classList.toggle('show')
+}
+js_h3[3].onclick = function () {
+    content[3].classList.toggle('show')
+}
+content[3].onclick = function () {
+    content[3].classList.toggle('show')
+}
+js_h3[4].onclick = function () {
+    content[4].classList.toggle('show')
+}
+content[4].onclick = function () {
+    content[4].classList.toggle('show')
+}
+js_h3[5].onclick = function () {
+    content[5].classList.toggle('show')
+}
+content[5].onclick = function () {
+    content[5].classList.toggle('show')
+}
+js_h3[6].onclick = function () {
+    content[6].classList.toggle('show')
+}
+content[6].onclick = function () {
+    content[6].classList.toggle('show')
+}
